adca662ec5015f467e5897f414a787de456df122
Andy Armstrong <andy@hexten.net>
2012-07-16 21:56:06 +0100

    Attempt to rotate the vehicle. Seems to expose a flaw in the reasoning behind wrapping elements inside nested <g>s to recentre them.

--- sim/Robot-Auton-Simulator/public/javascripts/sim.js
+++ sim/Robot-Auton-Simulator/public/javascripts/sim.js
@@ -4,8 +4,15 @@ $(function() {
     $('.m_wheel').each(function() {
       st.localise(this, $(this).hasClass('m_left') ? st.CR : st.CL);
     });
+    $('#m_vehicle').each(function() {
+      st.localise(this);
+    });
+
     $('.m_front').attr({
       transform: 'rotate(30)'
+    });
+    $('#m_vehicle').attr({
+      transform: 'rotate(-30)'
     });
   }
 
