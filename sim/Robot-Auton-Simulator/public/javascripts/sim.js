34cdc341c27d2f334d82e89e521f4849073990ca
Andy Armstrong <andy@hexten.net>
2012-07-16 21:06:56 +0100

    Helper class for SVG manipulation.

--- sim/Robot-Auton-Simulator/public/javascripts/sim.js
+++ sim/Robot-Auton-Simulator/public/javascripts/sim.js
@@ -1,29 +1,8 @@
 $(function() {
   function fixUp(svg) {
+    st = new SVGTool(svg);
     $('.m_wheel').each(function() {
-      var $this = $(this);
-      var bb = this.getBBox();
-      var cx = bb.x + ($this.hasClass('m_left') ? bb.width : 0);
-      var cy = bb.y + bb.height / 2;
-
-      var g1 = svg.other(this.parentNode, 'g', {
-        transform: 'translate(' + cx + ', ' + cy + ')'
-      });
-      var g2 = svg.other(g1, 'g', {
-        class: $this.attr('class'),
-        id: this.id
-      });
-      var g3 = svg.other(, 'g', {
-        transform: 'translate(' + -cx + ', ' + -cy + ')'
-      });
-
-      svg.remove(this);
-      $this.attr({
-        class: '',
-        id: ''
-      });
-      svg.add(g3, this);
-
+      st.localise(this, $(this).hasClass('m_left') ? st.CR : st.CL);
     });
     $('.m_front').attr({
       transform: 'rotate(30)'
---
+++ sim/Robot-Auton-Simulator/public/javascripts/svg.js
@@ -0,0 +1,42 @@
+function SVGTool(svg) {
+  this.svg = svg;
+}
+
+SVGTool.prototype = {
+  TL: 0x00,
+  TC: 0x01,
+  TR: 0x02,
+  CL: 0x10,
+  CC: 0x11,
+  CR: 0x12,
+  CL: 0x20,
+  CC: 0x21,
+  CR: 0x22,
+  anchor: function(bb, cent) {
+    return {
+      x: bb.x + bb.width * (cent & 0x0f) / 2,
+      y: bb.y + bb.height * ((cent >> 4) & 0x0f) / 2
+    };
+  },
+  localise: function(elt, cent) {
+    var ap = this.anchor(elt.getBBox(), cent);
+    var $elt = $(elt);
+    var g1 = this.svg.other(elt.parentNode, 'g', {
+      transform: 'translate(' + ap.x + ', ' + ap.y + ')'
+    });
+    var g2 = this.svg.other(g1, 'g', {
+      class: $elt.attr('class'),
+      id: elt.id
+    });
+    var g3 = this.svg.other(g2, 'g', {
+      transform: 'translate(' + -ap.x + ', ' + -ap.y + ')'
+    });
+
+    this.svg.remove(elt);
+    $elt.attr({
+      class: '',
+      id: ''
+    });
+    this.svg.add(g3, elt);
+  }
+}
