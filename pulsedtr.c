241b336931d2ff517080f56fb2a0e6093b7247ed
Andy Armstrong <andy@hexten.net>
2012-09-19 23:26:00 +0100

    Tidy.

--- pulsedtr.c
+++ pulsedtr.c
@@ -5,35 +5,33 @@
 #include <termios.h>
 #include <unistd.h>
 
-int setdtr( int fd, int on ) {
+int
+setdtr( int fd, int on ) {
   int controlbits = TIOCM_DTR;
-  if ( on )
-    return ioctl( fd, TIOCMBIC, &controlbits );
-  else
-    return ioctl( fd, TIOCMBIS, &controlbits );
+  return ioctl( fd, on ? TIOCMBIC : TIOCMBIS, &controlbits );
 }
 
-int main( int argc, char *argv[] ) {
+int
+main( int argc, char *argv[] ) {
   if ( argc != 2 ) {
     fprintf( stderr, "Please name a serial port (e.g. /dev/ttyUSB0)\n" );
-    return 1 ;
+    return 1;
   }
   const char *dev = argv[1];
   int fd = open( dev, O_RDWR );
   if ( fd < 0 ) {
     fprintf( stderr, "Couldn't open %s\n", dev );
-    return 1 ;
+    return 1;
   }
 
   fprintf( stderr, "Setting DTR\n" );
   setdtr( fd, 1 );
 
-  // Pause for three seconds
   sleep( 1 );
 
   fprintf( stderr, "Clearing DTR\n" );
   setdtr( fd, 0 );
 
   close( fd );
-  return 0 ;
+  return 0;
 }
