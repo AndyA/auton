545abf0173133e73cea8e12f58e52a4a5bec1097
Andy Armstrong <andy@hexten.net>
2012-09-22 00:33:42 +0100

    Not much better.

--- tank/brain.c
+++ tank/brain.c
@@ -21,15 +21,14 @@
 #define sgn(x) ((x) < 0 ? -1 : (x) > 0 ? 1 : 0)
 #endif
 
-static mavg prox_avg[2];
-static mavg turn_avg[2];
-static mavg drive_avg[2];
+static mavg prox_avg[1];
+
+static MAVG_DTYPE cturn, cdrive;
+static MAVG_DTYPE lprox;
 
 void
 br_init( void ) {
   mavg_init_multi( prox_avg, countof( prox_avg ) );
-  mavg_init_multi( turn_avg, countof( turn_avg ) );
-  mavg_init_multi( drive_avg, countof( drive_avg ) );
 }
 
 static MAVG_DTYPE
@@ -43,26 +42,23 @@ void
 br_update( uint16_t lprox, uint16_t rprox, int16_t * ldrive,
            int16_t * rdrive ) {
   MAVG_DTYPE prox = max( lprox, rprox );
-  MAVG_DTYPE base_prox = mavg_update( prox_avg, prox );
-  MAVG_DTYPE hot_prox = mavg_peek( &prox_avg[1] );
-  MAVG_DTYPE dprox = hot_prox - base_prox;
-  MAVG_DTYPE nturn = 0;
-  MAVG_DTYPE ndrive = 0;
+/*  MAVG_DTYPE base_prox = mavg_update( prox_avg, prox );*/
+/*  MAVG_DTYPE dprox = prox - base_prox;*/
+  MAVG_DTYPE dprox = prox - lprox;
+  lprox = prox;
 
   if ( dprox > 0 ) {
     MAVG_DTYPE turn = lprox - rprox;
     MAVG_DTYPE dir = sgn( turn );
     MAVG_DTYPE diff = abs( turn );
-    nturn = dprox * ( 20 - min( diff, 20 ) ) * dir * 4;
+
+    cturn = dprox * ( 30 - min( diff, 30 ) ) * dir;
   }
 
-  {
-    MAVG_DTYPE turn = mavg_update( turn_avg, nturn );
-    MAVG_DTYPE drive = max( 0, mavg_update( drive_avg, ndrive ) );
+  cturn -= sgn( cturn );
 
-    *ldrive = clip( -255, drive - turn, 255 );
-    *rdrive = clip( -255, drive + turn, 255 );
-  }
+  *ldrive = clip( -255, cdrive - cturn, 255 );
+  *rdrive = clip( -255, cdrive + cturn, 255 );
 }
 
 /* vim:ts=2:sw=2:sts=2:et:ft=c 
