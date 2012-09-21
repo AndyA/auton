2e4bfc052119b27c2acd59174b7eadb20409d112
Andy Armstrong <andy@hexten.net>
2012-09-22 00:10:06 +0100

    Emit tone.

--- range/range.cpp
+++ range/range.cpp
@@ -2,15 +2,16 @@
 
 void
 setup(  ) {
-  Serial.begin( 9600 );
+//  Serial.begin( 9600 );
 }
 
 void
 loop(  ) {
-  Serial.print( analogRead( 0 ) );
-  Serial.print( ", " );
-  Serial.println( analogRead( 1 ) );
-  delay( 500 );
+//  Serial.print( analogRead( 0 ) );
+//  Serial.print( ", " );
+//  Serial.println( analogRead( 1 ) );
+  tone( 8, analogRead( 0 ) );
+  delay( 40 );
 }
 
 int
