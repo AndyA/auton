4fcab4991f80f3fc5808805e427cfe1876de0f59
Andy Armstrong <andy@hexten.net>
2012-09-22 20:34:15 +0100

    Move stuff into brain.cpp

--- tank/brain.cpp
+++ tank/brain.cpp
@@ -1,5 +1,7 @@
 /* brain.c */
 
+#include "Arduino.h"
+
 #include "brain.h"
 #include "mavg.h"
 
@@ -21,14 +23,19 @@
 #define sgn(x) ((x) < 0 ? -1 : (x) > 0 ? 1 : 0)
 #endif
 
-static mavg prox_avg[1];
+#define FAR     60
+#define NEAR    210
+#define NEUTRAL 186
+
+static mavg lprox_avg[1], rprox_avg[1], turn_avg[2];
 
 static MAVG_DTYPE cturn, cdrive;
-static MAVG_DTYPE lprox;
+static MAVG_DTYPE pprox;
 
 void
 br_init( void ) {
-  mavg_init_multi( prox_avg, countof( prox_avg ) );
+  mavg_init_multi( lprox_avg, countof( lprox_avg ) );
+  mavg_init_multi( rprox_avg, countof( rprox_avg ) );
 }
 
 static MAVG_DTYPE
@@ -41,21 +48,51 @@ br__delta( mavg * m, MAVG_DTYPE d, uint8_t tap ) {
 void
 br_update( uint16_t lprox, uint16_t rprox, int16_t * ldrive,
            int16_t * rdrive ) {
-  MAVG_DTYPE prox = max( lprox, rprox );
-/*  MAVG_DTYPE base_prox = mavg_update( prox_avg, prox );*/
-/*  MAVG_DTYPE dprox = prox - base_prox;*/
-  MAVG_DTYPE dprox = prox - lprox;
-  lprox = prox;
+  MAVG_DTYPE slprox = mavg_update( lprox_avg, lprox );
+  MAVG_DTYPE srprox = mavg_update( rprox_avg, rprox );
+  MAVG_DTYPE prox = max( slprox, srprox );
+  MAVG_DTYPE dprox = prox - pprox;
+  MAVG_DTYPE turn = slprox - srprox;
+  MAVG_DTYPE sturn = mavg_update( turn_avg, turn );
+  MAVG_DTYPE dturn = turn - sturn;
+  pprox = prox;
 
   if ( dprox > 0 ) {
-    MAVG_DTYPE turn = lprox - rprox;
-    MAVG_DTYPE dir = sgn( turn );
-    MAVG_DTYPE diff = abs( turn );
+    MAVG_DTYPE dir = sgn( dturn );
+    MAVG_DTYPE diff = abs( dturn );
 
-    cturn = dprox * ( 30 - min( diff, 30 ) ) * dir;
+    MAVG_DTYPE adj = dprox * dprox * ( 100 - min( diff, 99 ) ) * dir / 100;
+    cturn = ( cturn + adj ) / 2;
+
+/*    Serial.print( "dprox=" );*/
+/*    Serial.print( dprox );*/
+/*    Serial.print( ", dturn=" );*/
+/*    Serial.print( dturn );*/
+/*    Serial.print( ", dir=" );*/
+/*    Serial.print( dir );*/
+/*    Serial.print( ", diff=" );*/
+/*    Serial.print( diff );*/
+/*    Serial.print( ", adj=" );*/
+/*    Serial.print( adj );*/
+/*    Serial.print( ", cturn=" );*/
+/*    Serial.println( cturn );*/
+  }
+
+  if ( prox > NEAR ) {
+    cdrive = -50;
+    cturn = 0;
+  }
+  else if ( prox > NEUTRAL ) {
+    cdrive /= 2;
+    cturn = sgn( dturn ) * 30;
+  }
+  else {
+    cdrive = min( 255, cdrive + 3 );
   }
 
   cturn -= sgn( cturn );
+/*  if ( cturn )*/
+/*    Serial.println( cturn );*/
 
   *ldrive = clip( -255, cdrive - cturn, 255 );
   *rdrive = clip( -255, cdrive + cturn, 255 );
--- tank/tank.cpp
+++ tank/tank.cpp
@@ -12,8 +12,8 @@
 #define OUT_RF  3
 #define OUT_RR  5
 
-#define RANGE_L 0
-#define RANGE_R 1
+#define RANGE_L 1
+#define RANGE_R 0
 
 #define CONTROL_MODE 13
 
@@ -21,9 +21,6 @@
 
 #define LEFT    IN_LF, IN_LR, OUT_LF, OUT_LR
 #define RIGHT   IN_RF, IN_RR, OUT_RF, OUT_RR
-#define FAR     60
-#define NEAR    195
-#define NEUTRAL 190
 
 static int16_t left, right;
 static int16_t speed;
@@ -51,35 +48,9 @@ set_channel( CHANNEL, int16_t speed ) {
   return speed;
 }
 
-static void
-update_channel( CHANNEL, int16_t * speed, uint16_t prox ) {
-  if ( digitalRead( in_f ) ) {
-    ( *speed )++;
-  }
-  else if ( digitalRead( in_r ) ) {
-    ( *speed )--;
-  }
-  else {
-    int16_t delta = NEUTRAL - prox;
-    if ( *speed < 0 && delta < 0 )
-      delta /= 3;
-    *speed += delta;
-  }
-
-  *speed = max( -255, min( *speed, 255 ) );
-
-  if ( *speed < 0 ) {
-    analogWrite( out_f, 0 );
-    analogWrite( out_r, -*speed );
-  }
-  else {
-    analogWrite( out_r, 0 );
-    analogWrite( out_f, *speed );
-  }
-}
-
 void
 setup(  ) {
+  Serial.begin( 38400 );
   br_init(  );
   pinMode( CONTROL_MODE, INPUT );
   init_channel( LEFT );
@@ -88,14 +59,18 @@ setup(  ) {
 
 void
 loop(  ) {
-  uint16_t lprox = analogRead( RANGE_R );
-  uint16_t rprox = analogRead( RANGE_L );
+  uint16_t lprox = analogRead( RANGE_L );
+  uint16_t rprox = analogRead( RANGE_R );
   int16_t ldrive, rdrive;
 
   br_update( lprox, rprox, &ldrive, &rdrive );
 
   set_channel( LEFT, ldrive );
   set_channel( RIGHT, rdrive );
+
+//  Serial.print( ldrive );
+//  Serial.print( "," );
+//  Serial.println( rdrive );
 
   delay( 40 );
 }
