d3170d31d4fec9ff63a8c60707189c5f02aa67fa
Andy Armstrong <andy@hexten.net>
2012-09-22 20:35:19 +0100

    Serial output, baud rate.

--- range/range.cpp
+++ range/range.cpp
@@ -2,15 +2,14 @@
 
 void
 setup(  ) {
-//  Serial.begin( 9600 );
+  Serial.begin( 38400 );
 }
 
 void
 loop(  ) {
-//  Serial.print( analogRead( 0 ) );
-//  Serial.print( ", " );
-//  Serial.println( analogRead( 1 ) );
-  tone( 8, analogRead( 0 ) );
+  Serial.print( analogRead( 0 ) );
+  Serial.print( "," );
+  Serial.println( analogRead( 1 ) );
   delay( 40 );
 }
 
