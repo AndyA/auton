0a41f59d1c2d83f2ced57041adf0908eb221277b
Andy Armstrong <andy@hexten.net>
2012-09-23 16:11:17 +0100

    Fast math.

---
+++ tank/imath.h
@@ -0,0 +1,13 @@
+/* imath.h */
+
+#ifndef __IMATH_H
+#define __IMATH_H
+
+#include <stdint.h>
+
+uint32_t isqrt( uint32_t n );
+
+#endif
+
+/* vim:ts=2:sw=2:sts=2:et:ft=c 
+ */
--- tank/observation.h
+++ tank/observation.h
@@ -4,20 +4,54 @@
 #define __OBSERVATION_H
 
 #include <stdint.h>
+#include "imath.h"
 
 template <class T, int N>
 class Observation {
 private:
   T ds[N];
   uint16_t pos, used;
+  uint8_t dirty;
+  T mean, rms;
+private:
+  T computeSum();
+  void compute();
+  void freshen();
 public:
-  Observation();
+  Observation(): pos( 0 ), used( 0 ) {}
   void push( T d );
 };
 
 template <class T, int N>
-Observation<T, N>::Observation() {
-  pos = used = 0;
+T Observation<T, N>::computeSum( ) {
+  T sum = 0;
+  uint16_t i;
+  for ( i = 0; i < N; i++ )
+    sum += ds[i];
+  return sum;
+}
+
+template <class T, int N>
+void Observation<T, N>::compute( ) {
+  uint32_t ms = 0;
+  uint16_t i;
+
+  mean = computeSum() / N;
+
+  for ( i = 0; i < N; i++ ) {
+    T dd = ds[i] - mean;
+    ms += dd * dd;
+  }
+
+  rms = isqrt( ms / N );
+}
+
+template <class T, int N>
+void Observation<T, N>::freshen( ) {
+  if ( dirty ) {
+    compute();
+    dirty = 0;
+  }
 }
 
 template <class T, int N>
@@ -25,6 +59,7 @@ void Observation<T, N>::push( T d ) {
   if ( pos == N ) pos = 0;
   ds[pos++] = d;
   if ( pos > used ) used = pos;
+  dirty = 1;
 }
 
 #endif
