b4b94a02ffc961b93c63b228ebe3f56b93c06892
Andy Armstrong <andy@hexten.net>
2012-09-23 17:32:30 +0100

    Remove unused.

--- tank/lib/brain.h
+++ tank/lib/brain.h
@@ -14,19 +14,7 @@ public:
   void update( int16_t lprox, int16_t rprox, int16_t *turn, int16_t *drive);
 };
 
-#ifdef __cplusplus
-extern "C" {
 #endif
 
-#include <stdint.h>
-
-  void br_init( void );
-  void br_update( uint16_t lprox, uint16_t rprox, int16_t * ldrive,
-                  int16_t * rdrive );
-
-#ifdef __cplusplus
-}
-#endif
-#endif
 /* vim:ts=2:sw=2:sts=2:et:ft=cpp
  */
