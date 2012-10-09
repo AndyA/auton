663a15e5a81f22a5e375c9caffc243032d63f8c3
Andy Armstrong <andy@hexten.net>
2012-10-09 01:39:45 +0100

    Spinning and sensing.

--- range/range.cpp
+++ range/range.cpp
@@ -1,24 +1,36 @@
 #include "Arduino.h"
+#include "Servo.h"
+
+#define LED 13
+#define IDX 2
+#define SPIN 10
+
+Servo spin;
 
 void
-setup(  ) {
-  Serial.begin( 38400 );
+setup() {
+//  Serial.begin( 38400 );
+  pinMode(LED, OUTPUT);
+  pinMode(IDX, INPUT);
+  spin.attach(SPIN);
+  spin.write(90);
 }
 
 void
-loop(  ) {
-  Serial.print( analogRead( 0 ) );
-  Serial.print( "," );
-  Serial.println( analogRead( 1 ) );
-  delay( 40 );
+loop() {
+  digitalWrite(LED, digitalRead(IDX) ? LOW : HIGH);
+//  Serial.print( analogRead( 0 ) );
+//  Serial.print( "," );
+//  Serial.println( analogRead( 1 ) );
+//  delay( 40 );
 }
 
 int
-main( void ) {
-  init(  );
-  setup(  );
-  for ( ;; ) {
-    loop(  );
+main(void) {
+  init();
+  setup();
+  for (;;) {
+    loop();
   }
   return 0;
 }
