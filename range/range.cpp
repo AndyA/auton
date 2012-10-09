cfd5665056f15efc0f9bc80583723e5c460081a4
Andy Armstrong <andy@hexten.net>
2012-10-09 02:05:28 +0100

    Working (but poor) speed control.

--- range/range.cpp
+++ range/range.cpp
@@ -5,24 +5,48 @@
 #define IDX 2
 #define SPIN 10
 
+#define DESIRED 550
+
 Servo spin;
 
-void
-setup() {
-//  Serial.begin( 38400 );
+unsigned long last_idx;
+unsigned long period;
+unsigned int throttle = 100;
+
+unsigned long last_period, last_throttle;
+
+void index_int() {
+  unsigned long now = millis();
+
+  period = now - last_idx;
+  last_idx = now;
+
+  if (period > DESIRED && throttle < 180)
+    throttle++;
+  else if (period < DESIRED && throttle > 0)
+    throttle--;
+
+  spin.write(throttle);
+}
+
+void setup() {
+  Serial.begin(38400);
   pinMode(LED, OUTPUT);
   pinMode(IDX, INPUT);
   spin.attach(SPIN);
-  spin.write(90);
+  spin.write(throttle);
+  attachInterrupt(0, index_int, FALLING);
 }
 
-void
-loop() {
+void loop() {
   digitalWrite(LED, digitalRead(IDX) ? LOW : HIGH);
-//  Serial.print( analogRead( 0 ) );
-//  Serial.print( "," );
-//  Serial.println( analogRead( 1 ) );
-//  delay( 40 );
+  if (last_period != period || last_throttle != throttle) {
+    Serial.print(period);
+    Serial.print(", ");
+    Serial.println(throttle);
+    last_period = period;
+    last_throttle = throttle;
+  }
 }
 
 int
